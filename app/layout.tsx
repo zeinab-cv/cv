71a2f5502fba9a76fa2cc9e2de0f2fb3ec9d888c
Toni Rajamäki <zingoo16@gmail.com>
2025-06-20 11:19:46 +0300

    Add favicon to layout and include new logo image; update layout metadata for improved branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/zd-logo.jpg" type="image/jpeg" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
